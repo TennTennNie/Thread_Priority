e2e785369eda0b059d4dcbac1c1475ccdbd00ae9
TennTennNie <144678351+TennTennNie@users.noreply.github.com>
2023-10-23 17:43:17 +0800

    Add files via upload

---
+++ FrmBasicThreads/FrmBasicThreads/FrmBasicThreads.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace FrmBasicThreads
+{
+    partial class FrmBasicThreads
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.label1.Location = new System.Drawing.Point(58, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "-Basic Starting Thread-";
+            // 
+            // btnRun
+            // 
+            this.btnRun.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnRun.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btnRun.Location = new System.Drawing.Point(137, 134);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(84, 35);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = false;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // FrmBasicThreads
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(354, 207);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicThreads";
+            this.Text = "Basic Thread";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ FrmBasicThreads/FrmBasicThreads/FrmBasicThreads.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThreads
+{
+    public partial class FrmBasicThreads : Form
+    {
+        public FrmBasicThreads()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before Starting Thread-");
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            Thread threadB = new Thread(MyThreadClass.Thread2);
+            Thread threadC = new Thread(MyThreadClass.Thread1);
+            Thread threadD = new Thread(MyThreadClass.Thread2);
+
+            threadA.Start();
+            threadA.Name = "Thread A in Process";
+
+            threadB.Start();
+            threadB.Name = "Thread B in Process";
+
+            threadC.Start();
+            threadC.Name = "Thread C in Process";
+
+            threadD.Start();
+            threadD.Name = "Thread D in Process";
+            
+            threadA.Priority = ThreadPriority.Highest;
+            threadB.Priority = ThreadPriority.Normal;
+            threadC.Priority = ThreadPriority.AboveNormal;
+            threadD.Priority = ThreadPriority.BelowNormal;
+
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            Console.WriteLine("-End of Thread-");
+
+            label1.Text = "-End of Thread-";
+        }
+    }
+}
